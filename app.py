f362b439a3e1929a756521f2024a7533dc9c9d22
Ragul Srinivasan <140724027+Ragulvasan08@users.noreply.github.com>
2024-09-17 22:59:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import streamlit as st
 import pandas as pd
 
 st.title('😴 Sleep Score Prediction APP')
+
+st.info('This is developed by using the Machine Learning Algorithm🤖')
 data = pd.read_csv('https://raw.githubusercontent.com/Ragulvasan08/Sleep_Score_Prediction/refs/heads/main/Sleep_Quality.csv')
 data
+
+
 
 
