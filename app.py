5a67917096e0607511f66da62f7c41a755334a3a
Ragul Srinivasan <140724027+Ragulvasan08@users.noreply.github.com>
2024-09-17 23:01:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import pandas as pd
 st.title('😴 Sleep Score Prediction APP')
 
 st.info('This is developed by using the Machine Learning Algorithm🤖')
-data = pd.read_csv('https://raw.githubusercontent.com/Ragulvasan08/Sleep_Score_Prediction/refs/heads/main/Sleep_Quality.csv')
-data
+
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  data = pd.read_csv('https://raw.githubusercontent.com/Ragulvasan08/Sleep_Score_Prediction/refs/heads/main/Sleep_Quality.csv')
+  data
 
 
 
